14e9df40b2fc721a29fc4b1e65ec35460b60ec31
qianxiaoxiao <121172722@qq.com>
2019-12-30 14:46:49 +0800

    fix main

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	_ "blog/mvc/web"
-	"blog/mvc/zz"
 	"blog/system"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"github.com/ltto/T/www"
 )
 
 func main() {
 	gin.SetMode("release")
-	if err := zz.Engine.Run(fmt.Sprintf("127.0.0.1:%d", system.Conf.Server.Port)); err != nil {
+	if err := www.Engine.Run(fmt.Sprintf("127.0.0.1:%d", system.Conf.Server.Port)); err != nil {
 		panic(err)
 	}
 }
